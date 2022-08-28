b3671647cae1082345d3e777f915a1897dea12f4
Bruno Lemes <bruno.santos@climatempo.io>
2022-08-28 12:05:51 -0300

    feat: added widget materialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
 
+const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: darkBlue,
+      ),
+    );
+  }
+}
