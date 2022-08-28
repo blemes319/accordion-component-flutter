7436b016e70056c32b3a566f5d38d3856e8e15ce
Bruno Lemes <bruno.santos@climatempo.io>
2022-08-28 12:47:09 -0300

    chore: move component accordion

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'components/accordion_component.dart';
 
 const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
 
@@ -52,32 +53,6 @@ class MyApp extends StatelessWidget {
             })
           ],
         )),
-      ),
-    );
-  }
-}
-
-class Accordion extends StatelessWidget {
-  String? content, title;
-  late bool showContent;
-
-  Accordion({this.showContent = false, this.content, this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    print(showContent);
-    return Container(
-      width: 150,
-      height: 100,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('${title}'),
-          Visibility(
-            visible: showContent,
-            child: Text('${content}'),
-          )
-        ],
       ),
     );
   }
