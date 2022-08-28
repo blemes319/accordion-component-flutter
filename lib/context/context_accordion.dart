c557c8998c7e2c1cfd7d38e46ca27e15622ea148
Bruno Lemes <bruno.santos@climatempo.io>
2022-08-28 19:07:30 -0300

    feat: added id accordion

--- lib/context/context_accordion.dart
+++ lib/context/context_accordion.dart
@@ -2,18 +2,21 @@ import 'package:flutter/material.dart';
 
 List<Map<String, dynamic>> lista = [
   {
+    'id': 1,
     'content': 'Blá blá blá blá blá blá blá blá ',
     'title': 'Teste do teste 1',
     'color': Color.fromARGB(0, 47, 35, 204).withOpacity(1),
     'active': false,
   },
   {
+    'id': 2,
     'content': 'Blá blá blá blá blá blá blá blá ',
     'title': 'Teste do teste 2',
     'color': Color.fromARGB(0, 202, 124, 46).withOpacity(1),
     'active': true,
   },
   {
+    'id': 3,
     'content':
         'Blá bláBlá blá blá blá blá blá blá bláBlá blá blá blá blá blá blá blá',
     'title': 'Teste do teste 3',
@@ -21,9 +24,18 @@ List<Map<String, dynamic>> lista = [
     'active': true,
   },
   {
+    'id': 4,
     'content': 'Blá blá blá blá blá blá blá blá ',
     'title': 'Teste do teste 4',
     'color': Color.fromARGB(0, 150, 7, 36).withOpacity(1),
     'active': false,
   }
 ];
+
+// List<Map<String, dynamic>> change({int value= 0}) {
+//   return lista.map((list) {
+//     if(list){
+
+//     }
+//   });
+// }
