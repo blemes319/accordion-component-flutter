f4cf7fdbf03d58777337f98fcda0efe25b15e375
Bruno Lemes <bruno.santos@climatempo.io>
2022-08-28 12:39:11 -0300

    feat: added object layout acordion

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,29 @@ import 'package:flutter/material.dart';
 
 const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
 
+List<Map<String, dynamic>> lista = [
+  {
+    'content': 'Blá blá blá blá blá blá blá blá ',
+    'title': 'Teste do teste 1',
+    'active': false,
+  },
+  {
+    'content': 'Blá blá blá blá blá blá blá blá ',
+    'title': 'Teste do teste 2',
+    'active': false,
+  },
+  {
+    'content': 'Blá blá blá blá blá blá blá blá ',
+    'title': 'Teste do teste 3',
+    'active': true,
+  },
+  {
+    'content': 'Blá blá blá blá blá blá blá blá ',
+    'title': 'Teste do teste 4',
+    'active': false,
+  }
+];
+
 void main() {
   runApp(MyApp());
 }
