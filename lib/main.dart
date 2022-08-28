009f2a6cd1d584d5a69b82776659a278daa03673
Bruno Lemes <bruno.santos@climatempo.io>
2022-08-28 12:08:59 -0300

    feat: added Accordion component initial

--- lib/main.dart
+++ lib/main.dart
@@ -16,3 +16,29 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class Accordion extends StatelessWidget {
+  String? content, title;
+  late bool showContent;
+
+  Accordion({this.showContent = false, this.content, this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    print(showContent);
+    return Container(
+      width: 150,
+      height: 100,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('${title}'),
+          Visibility(
+            visible: showContent,
+            child: Text('${content}'),
+          )
+        ],
+      ),
+    );
+  }
+}
